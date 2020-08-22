0e6fa059180ca5ab2a761cd10a411bf38a3b1a8c
oleh-turchyn <47058088+Leggi19@users.noreply.github.com>
2020-08-22 17:42:01 +0300

    task7 lazy BeanF init

--- src/main/java/com/turchyn/hometask/configurations/AnotherConfiguration.java
+++ src/main/java/com/turchyn/hometask/configurations/AnotherConfiguration.java
@@ -1,10 +1,16 @@
 package com.turchyn.hometask.configurations;
 
-import com.turchyn.hometask.pojo.BeanE;
+import com.turchyn.hometask.pojo.*;
+import com.turchyn.hometask.pojo.BeanF;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
-//@Configuration
+@Configuration
 public class AnotherConfiguration {
-
+    @Bean
+    @Lazy
+    public BeanF beanF(){
+        return new BeanF();
+    }
 }
