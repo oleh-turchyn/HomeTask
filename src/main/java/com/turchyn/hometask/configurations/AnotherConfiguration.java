28bc4222db5d346f0ea7581ea069fb9c21188157
oleh-turchyn <47058088+Leggi19@users.noreply.github.com>
2020-08-23 18:17:15 +0300

    p2 task5 create new package with other beans

--- src/main/java/com/turchyn/hometask/configurations/AnotherConfiguration.java
+++ src/main/java/com/turchyn/hometask/configurations/AnotherConfiguration.java
@@ -1,7 +1,6 @@
 package com.turchyn.hometask.configurations;
 
 import com.turchyn.hometask.pojo.beans2.CatAnimal;
-import com.turchyn.hometask.pojo.beans2.NarcissusFlower;
 import com.turchyn.hometask.pojo.beans3.BeanE;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
---
+++ src/main/java/com/turchyn/hometask/pojo/anotherbeans/OtherBeanB.java
@@ -0,0 +1,8 @@
+package com.turchyn.hometask.pojo.anotherbeans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class OtherBeanB {
+}
